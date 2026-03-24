4a712e9c622f94b9e11b4896f3c4c6624d589b23
Alejandro Lorenzo <alormel012@g.educaand.es>
2026-03-24 11:41:49 +0100

    Finish goal script to change scene

--- scenes/levels/Goal.gd
+++ scenes/levels/Goal.gd
@@ -1,8 +1,9 @@
 extends Area2D
-
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		GameManager.complete_level()
+	body_entered.connect(func(b: Node):
+		if b.is_in_group("player"):
+			var level = get_tree().get_first_node_in_group("level")
+			if level:
+				GameManager.save_run(level.score, level.distance, level.bones_collected)
+			GameManager.complete_level()
+	)
